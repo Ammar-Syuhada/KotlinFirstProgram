4b24348dc7253995e706f767521997913c555b57
Ammar-Syuhada <mhdammarsyuhada@gmail.com>
2025-09-19 21:18:05 +0700

    Percobaan Val dan Val

---
+++ src/main/kotlin/ValVariabel.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var varVariabel = 56 //assigning first value to varVariabel
+    println("Pemberian nilai pertama pada varVariabel = " +varVariabel)
+    varVariabel = 78 //assigning second value to varVariabel
+
+    val  phi = 3.14
+//    phi 2.5
+}
