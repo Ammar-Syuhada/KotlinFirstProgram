5b6bda63fb175cbec3e2fb2f7e07e28077632744
Ammar-Syuhada <mhdammarsyuhada@gmail.com>
2025-09-19 21:17:34 +0700

    Percobaan Tipe Data bilangan bulat dan decimal

---
+++ src/main/kotlin/TipeData.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    val  minByte: Byte = -128
+    val maxByte: Byte = 127 //8bit
+    val  minShort: Short = -32768
+    val  maxShort: Short = 32767 //16bit
+    val minInt: Int = -2147483648
+    val maxInt: Int = 2147483647 //32bit
+    val minLong: Long = -9223372036854775807
+    val maxLong: Long = 9223372036854775807 //64bit
+
+    println("MinByte: " +minByte)
+    println("MaxByte: " +maxByte)
+    println("MinShort: " +minShort)
+    println("MaxShort: " +maxShort)
+    println("MinInt: " +minInt)
+    println("MaxInt: " +maxInt)
+    println("MinLong: " +minLong)
+    println("MaxLong: " +maxLong)
+
+}
