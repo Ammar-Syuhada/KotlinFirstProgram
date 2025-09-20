550f2c51169854ef22a1721f655d086923ca011e
Ammar-Syuhada <mhdammarsyuhada@gmail.com>
2025-09-20 08:12:39 +0700

    Percobaan String

---
+++ src/main/kotlin/String.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val myName: String = "Mhd. Ammar Syuhada"
+    for (chr in myName) {
+        print(chr)
+    }
+    print('\n')
+
+    val s = "abc" + 1
+    println(s + "def")
+}
